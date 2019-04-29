0eb974a3adee56394f0ec25b3e1bc1df7626a44d
edodf <edofirmansyah23@gmail.com>
2019-04-29 14:21:56 +0700

    konek

---
+++ src/Menu/konek.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Menu;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class konek {
+    public static Connection con;
+    public static Statement stm;
+    public static Connection konekDB()throws SQLException{
+        
+        try {
+            String url ="jdbc:mysql://localhost:3306/logact";
+            String user="root";
+            String pass="";
+            Class.forName("com.mysql.jdbc.Driver");
+            con =DriverManager.getConnection(url,user,pass);
+            stm = con.createStatement();
+            System.out.println("koneksi berhasil;");
+        } catch (Exception e) {
+            System.err.println("koneksi gagal" +e.getMessage());
+        }
+        return con;
+    }
+}
