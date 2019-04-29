9ce4c2239613ca0c80158e6bf45ab5443e4af089
edodf <48007138+edodf@users.noreply.github.com>
2019-04-29 14:32:36 +0700

    basdat jadwal dan tugas

---
+++ logact.sql
@@ -0,0 +1,127 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 28 Apr 2019 pada 15.01
+-- Versi server: 10.1.38-MariaDB
+-- Versi PHP: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `logact`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jadwal`
+--
+
+CREATE TABLE `jadwal` (
+  `no_jadwal` int(5) NOT NULL,
+  `nama_jadwal` char(12) NOT NULL,
+  `hari` char(10) NOT NULL,
+  `mata_kuliah` char(12) NOT NULL,
+  `ruang` char(10) NOT NULL,
+  `waktu_mulai` char(5) NOT NULL,
+  `waktu_selesai` char(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `jadwal`
+--
+
+INSERT INTO `jadwal` (`no_jadwal`, `nama_jadwal`, `hari`, `mata_kuliah`, `ruang`, `waktu_mulai`, `waktu_selesai`) VALUES
+(1, 'tubes', 'SELASA', 'pbo', 'e102', '09.00', '11.40'),
+(2, 'ja', 'SELASA', 'pbo', 'e102', '09.00', '11.40'),
+(3, 'nub', 'SABTU', 'pbo', 'e306', '17.00', '19.30');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `login`
+--
+
+CREATE TABLE `login` (
+  `username` varchar(30) NOT NULL,
+  `password` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `register`
+--
+
+CREATE TABLE `register` (
+  `email` varchar(30) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `register`
+--
+
+INSERT INTO `register` (`email`, `nama`, `username`, `password`) VALUES
+('edofirmansyah23@gmail.com', 'edo df', 'edodf', 14117021);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tugas`
+--
+
+CREATE TABLE `tugas` (
+  `no_tugas` int(5) NOT NULL,
+  `nama_tugas` varchar(50) NOT NULL,
+  `mata_kuliah` varchar(30) NOT NULL,
+  `hari_pengumpulan` char(10) NOT NULL,
+  `waktu_pengumpulan` char(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD PRIMARY KEY (`no_jadwal`);
+
+--
+-- Indeks untuk tabel `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`password`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indeks untuk tabel `register`
+--
+ALTER TABLE `register`
+  ADD PRIMARY KEY (`password`);
+
+--
+-- Indeks untuk tabel `tugas`
+--
+ALTER TABLE `tugas`
+  ADD PRIMARY KEY (`no_tugas`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
